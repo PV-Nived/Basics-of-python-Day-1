3a6c06bef9ec0f50166ae821c71ed0801dee6e66
PV-Nived <nanduavadi@gmail.com>
2026-09-22 23:14:04 +0530

    Add files via upload

---
+++ Day 1 - Basics of python.py
@@ -0,0 +1,21 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #type conversion
+>>> A=100
+>>> type(A)
+<class 'int'>
+>>> float(A)
+100.0
+>>> 
+>>> S= str(A)
+>>> print(S)
+100
+>>> 
+>>> type(S)
+<class 'str'>
+>>> A
+100
+>>> S
+'100'
+>>> float(S)
+100.0
